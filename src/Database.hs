3dfa09eac669fe73d02746d2423af03df0f01071
Majeed12345321 <abdulmageed2233@gmail.com>
2020-12-01 15:58:20 +0000

    test

--- src/Database.hs
+++ src/Database.hs
@@ -56,7 +56,7 @@ initialiseDB = do
 -- storeHolidays conn xs = do
 
 
--- This function will insert the holiday records into the database
+-- This function will insert the holiday records into the database majeed
 insertDB :: Connection -> [HolidayRecord] -> IO ()
 insertDB conn records = do
   let xs = records -- need to use records and produce xs, this seems easiest possibility
